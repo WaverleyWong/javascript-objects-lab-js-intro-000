bf5960b8424a7b36e0928a2f7989b9115ce2b8a6
Waverley Wong <waverleybrontewong@gmail.com>
2018-11-11 23:48:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,8 @@ function destructivelyUpdateObjectWithKeyAndValue (object, key, value){
   return object
 }
 
-function deleteFromObjectByKey(object, key)
+function deleteFromObjectByKey(object, key){
+  const newObj = Object.assign({}, object)
+  delete newObj[key]
+  return newObj
+}

41684a360ba5c3ec10f1869a24d9ba7e5859b2f3
Waverley Wong <waverleybrontewong@gmail.com>
2018-11-11 23:41:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
-function updateObjectwith
+function updateObjectwithKetAndValue(targetObject, updatesObject) {
+  return object.assign({}), targetObject, updatesObject)
+}
